55535dc75ea31afd802b5cb7fb0ec0eecda01971
Rodrigo Passos <rodrigopassosnascimento@gmail.com>
2024-01-22 12:29:11 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+import Perfil from "./assets/components/Perfil";
+import Formulario from "./assets/components/Formulario";
+import Reposlist from "./assets/components/ReposList";
+
+function App() {
+  const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+
+  return (
+    <>
+      Digite o usuário Github: <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)}/>
+
+      {nomeUsuario.length > 4 && (
+        <>
+          <Perfil nomeUsuario={nomeUsuario} />      
+          <Reposlist nomeUsuario={nomeUsuario}/>
+        </>
+      )}
+
+      {/* {formularioEstaVisivel && (
+        <Formulario />
+      )}
+
+      <button onClick={() => setFormularioEstaVisivel(!formularioEstaVisivel)} type="button">toggle form</button> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Perfil/index.jsx
@@ -0,0 +1,21 @@
+import styles from './Perfil.module.css';
+
+const Perfil = ({nomeUsuario}) => {
+    // const usuario = {
+    //     nome: 'Rodrigo Passos',
+    //     avatar: 'https://github.com/rodripassos.png'
+    // }
+
+    return (
+        <header className={styles.header}>
+            {/* <img className='perfil-avatar' src={usuario.avatar}/>
+            <h3 className='perfil-titulo'>{usuario.nome}</h3> */}
+            <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`}/>
+            <h1 className={styles.name}>
+                {nomeUsuario}
+            </h1>
+        </header>
+    )
+}
+
+export default Perfil;
